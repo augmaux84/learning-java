59062440711ca496d337873bc5725dc31dc6643d
Augusto Andrei de Melo Maux <augusto.maux@escolar.ifrn.edu.br>
2026-07-27 10:53:36 -0300

    feat: Adding the first code in the Java language

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age = 17;
+        double weight = 59.00;
+        char K = 'K';
+        char g = 'g';
+        boolean isStudent = true;
+
+        if (isStudent) {
+            String student = "Augusto";
+            System.out.println("Hello, " + student + "!");
+            System.out.println("You are a student!");
+
+        } else {
+            String person = "Maria";
+            System.out.println("You aren't a student " + person + "!");
+        }
+    }
+
+}
