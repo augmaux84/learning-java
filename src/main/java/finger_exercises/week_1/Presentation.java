e576dbe1f961d70e85d487d8abfbdc67c48350d2
augmaux84 <augustoandrei190909@gmail.com>
2026-08-03 11:03:10 -0300

    feat: adding a simple exercise on variables and outputs

---
+++ src/main/java/finger_exercises/week_1/Presentation.java
@@ -0,0 +1,15 @@
+package finger_exercises.week_1;
+
+public class Presentation {
+    public static void main(String[] args) {
+        String name = "Augusto";
+        int age = 17;
+        double height = 1.70;
+
+        System.out.print("Name: " + name + "\nAge: " + age + "\nHeight: " + height);
+
+        // System.out.println("Name: " + name);
+        // System.out.println("Age: " + age);
+        // System.out.println("Height: " + height);
+    }
+}
--- src/main/java/saints_quiz/SaintsQuiz.java
+++ src/main/java/saints_quiz/SaintsQuiz.java
@@ -1,3 +1,5 @@
+package saints_quiz;
+
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
