1d879d7369039166853248b0474209ad44901b65
augmaux84 <augustoandrei190909@gmail.com>
2026-08-07 07:25:39 -0300

    feat: adding an exercise with increasing count using a for loop

---
+++ src/main/java/finger_exercises/week_3/AscendingCount.java
@@ -0,0 +1,9 @@
+package finger_exercises.week_3;
+
+public class AscendingCount {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++) { // or (int i=1; i<11; i++)
+            System.out.println(i);
+        }
+    }
+}
