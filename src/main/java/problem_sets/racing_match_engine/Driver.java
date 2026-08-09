a285352269ffa92d3a1c9e93d4cb23dc219728c7
augmaux84 <augustoandrei190909@gmail.com>
2026-08-09 13:10:46 -0300

    feat: create a Driver class with attributes and a constructor using this

---
+++ src/main/java/problem_sets/racing_match_engine/Driver.java
@@ -0,0 +1,24 @@
+package problem_sets.racing_match_engine;
+
+public class Driver {
+    String name;
+    String carModel;
+    double rating;
+    boolean isAvailable;
+
+    public Driver(String name, String carModel, double rating, boolean isAvailable) {
+        this.name = name;
+        this.carModel = carModel;
+        this.rating = rating;
+        this.isAvailable = isAvailable;
+    }
+
+//    public static void acceptRide(Driver driver) {
+//        if (driver.isAvailable) {
+//            driver.isAvailable = false;
+//            System.out.printf("%s accepted the ride on the %s", driver.name, driver.carModel);
+//        } else {
+//            System.out.printf("The %s driver is not available.", driver.name);
+//        }
+//    }
+}
