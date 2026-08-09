c3bfbdaab099088232e99b5463db087b7e5a2ce2
augmaux84 <augustoandrei190909@gmail.com>
2026-08-09 18:26:47 -0300

    refactor: applying this and removing parameters from acceptRide

--- src/main/java/problem_sets/racing_match_engine/Driver.java
+++ src/main/java/problem_sets/racing_match_engine/Driver.java
@@ -13,31 +13,16 @@ public class Driver {
     public Driver(String name, String carModel, double rating, boolean isAvailable) {
         this.name = name;
         this.carModel = carModel;
-
         if (rating < 0 || rating > 5) {
             this.rating = 5.0;
         } else {
             this.rating = rating;
         }
-
         this.isAvailable = isAvailable;
     }
 
-    public static void acceptRide(ArrayList<Driver> arrayList) {
-        int counter = 0;
-        for (Driver driver : arrayList) {
-            if (driver.isAvailable) {
-                driver.isAvailable = false;
-                System.out.printf("%s accepted the ride on the %s", driver.name, driver.carModel);
-                counter++;
-                break;
-            } else {
-                continue;
-            }
-        }
-
-        if (counter == 0) {
-            System.out.println("No driver is available... Try again in a few moments.");
-        } else {}
+    public void acceptRide() {
+        this.isAvailable = false;
+        System.out.printf("%s accepted the ride on the %s.", this.name, this.carModel);
     }
 }
--- src/main/java/problem_sets/racing_match_engine/UberApp.java
+++ src/main/java/problem_sets/racing_match_engine/UberApp.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class UberApp {
     public static void main(String[] args) {
         Driver firstDriver = new Driver("Tomás", "Chevrolet Onix", 5.0, false);
-        Driver secondDriver = new Driver("José", "Chevrolet Tracker", 5.0, false);
+        Driver secondDriver = new Driver("José", "Chevrolet Tracker", 5.0, true);
         Driver thirdDriver = new Driver("Augusto", "Fiat Uno", 4.0, false);
 
         ArrayList<Driver> arrayListDriver = new ArrayList<>();
@@ -14,6 +14,19 @@ public class UberApp {
         arrayListDriver.add(secondDriver);
         arrayListDriver.add(thirdDriver);
 
-        Driver.acceptRide(arrayListDriver);
+//        Driver.acceptRide(arrayListDriver);
+
+        int counter = 0;
+        for (Driver driver : arrayListDriver) {
+            if (driver.isAvailable) {
+                driver.acceptRide();
+                counter++;
+                break;
+            }
+        }
+
+        if (counter == 0) {
+            System.out.println("No driver is available... Try again in a few moments.");
+        }
     }
 }
