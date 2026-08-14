4930bdc7d59de625a84cf5efba82f9a3481dac1d
augmaux84 <augustoandrei190909@gmail.com>
2026-08-14 09:09:32 -0300

    fix: adjusting the return structure of processTransaction

--- src/main/java/problem_sets/polymorphic_payment_engine/AbstractPayment.java
+++ src/main/java/problem_sets/polymorphic_payment_engine/AbstractPayment.java
@@ -8,6 +8,16 @@ public abstract class AbstractPayment implements IPayable{
     protected abstract boolean validate();
 
     public boolean processTransaction() {
-        return false;
+        if (!status.equalsIgnoreCase("PENDING")) {
+            System.out.println("Validation already performed");
+            return false;
+        } else if (this.validate()) {
+            this.status = "SUCCESS";
+            return true;
+        } else {
+            this.status = "FAILED";
+            System.out.println("Validation Error");
+            return false;
+        }
     }
 }
