3cf220bb200a228508f7c75aa030db530066d5d0
augmaux84 <augustoandrei190909@gmail.com>
2026-08-15 20:36:29 -0300

    test: creating MercadoPagoApp and executing polymorphic tests on the ArrayList

--- src/main/java/problem_sets/polymorphic_payment_engine/MercadoPagoApp.java
+++ src/main/java/problem_sets/polymorphic_payment_engine/MercadoPagoApp.java
@@ -1,14 +1,22 @@
 package problem_sets.polymorphic_payment_engine;
 
-public class Main {
+import java.util.ArrayList;
+
+public class MercadoPagoApp {
     public static void main(String[] args) {
         PixPayment pixPayment = new PixPayment("TX-1001", 1, "7b9e4a2c-1f8d-4e3b-9a0c-2f5d8e1b3a4f");
-        pixPayment.processTransaction();
 
         CreditCardPayment creditCardPayment = new CreditCardPayment("TX-1002", 50.2, "4756291038475621", 15000);
-        creditCardPayment.processTransaction();
 
         BoletoPayment boletoPayment = new BoletoPayment("TX-1003", 30, "34191790010435100479100184310005191026054000001");
-        boletoPayment.processTransaction();
+
+        ArrayList<AbstractPayment> arrayList = new ArrayList<>();
+        arrayList.add(pixPayment);
+        arrayList.add(creditCardPayment);
+        arrayList.add(boletoPayment);
+
+        for (AbstractPayment list : arrayList) {
+            list.processTransaction();
+        }
     }
 }
