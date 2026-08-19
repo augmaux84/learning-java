8af909b6b02cbac8cd6b107e3fdadc884cc94a06
augmaux84 <augustoandrei190909@gmail.com>
2026-08-19 18:49:25 -0300

    feat: implement Product domain entity with deductStock method

---
+++ src/main/java/problem_sets/inventory_system/Product.java
@@ -0,0 +1,29 @@
+package problem_sets.inventory_system;
+
+public class Product {
+    private final String id;
+    private final String name;
+    private int stockQuantity; // why can't I use the static here?
+
+    public Product(String id, String name, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void deductStock(int amount) {
+        stockQuantity -= amount;
+    }
+}
