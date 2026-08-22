65a6a30a0cd0a9597b734e4d7a086223ebd84862
augmaux84 <augustoandrei190909@gmail.com>
2026-08-22 09:13:14 -0300

    feat: adding an exercise about second calculator test

---
+++ src/main/java/finger_exercises/week_9/SecondCalculatorTest.java
@@ -0,0 +1,15 @@
+package finger_exercises.week_9;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SecondCalculatorTest {
+    public int doubleNumbers(int number) {
+        return number * 2;
+    }
+
+    @Test
+    public void testDouble() {
+        assertEquals(4, doubleNumbers(2));
+    }
+}
