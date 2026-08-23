93b88649f11e34dc2c785d1582a73563cd627d03
augmaux84 <augustoandrei190909@gmail.com>
2026-08-23 17:42:19 -0300

    feat: adding the SongNotFoundException

---
+++ src/main/java/problem_sets/collection_manipulation/SongNotFoundException.java
@@ -0,0 +1,7 @@
+package problem_sets.collection_manipulation;
+
+public class SongNotFoundException extends Exception{
+    public SongNotFoundException(String message) {
+        super(message);
+    }
+}
