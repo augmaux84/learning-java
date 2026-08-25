2b5be23f1ae79bf90a0ffce266ad4cf47a55f47c
augmaux84 <augustoandrei190909@gmail.com>
2026-08-25 08:07:15 -0300

    feat: adding the findSongByTitle method

--- src/main/java/problem_sets/collection_manipulation/Playlist.java
+++ src/main/java/problem_sets/collection_manipulation/Playlist.java
@@ -6,7 +6,7 @@ public class Playlist {
     private String name;
     private ArrayList<Song> songs;
 
-    public void Playlist(String name) {
+    public Playlist(String name) {
         this.name = name;
         this.songs = new ArrayList<>();
     }
@@ -41,6 +41,20 @@ public class Playlist {
             System.out.printf("Error: The song :.2f is already in the playlist.", song.getTitle());
         } else {
             this.songs.add(song);
+        }
+    }
+
+    public Song findSongByTitle(String title) throws SongNotFoundException {
+        int counter = 0;
+        for (Song song : this.songs) {
+            if (song.getTitle().equalsIgnoreCase(title)) {
+                counter = 1;
+                break;
+            }
+        }
+
+        if (counter == 0) {
+            throw new SongNotFoundException("Song not found: " + title);
         }
     }
 }
