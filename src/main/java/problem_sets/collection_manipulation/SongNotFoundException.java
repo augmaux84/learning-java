1d4520076cf5864dd5060bf5c198d81d56a9fe23
augmaux84 <augustoandrei190909@gmail.com>
2026-08-31 14:03:06 -0300

    remove: removing collection handling folder

--- src/main/java/problem_sets/collection_manipulation/SongNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package problem_sets.collection_manipulation;
-
-public class SongNotFoundException extends Exception{
-    public SongNotFoundException(String message) {
-        super(message);
-    }
-}
